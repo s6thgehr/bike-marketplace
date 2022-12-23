5e94c63a16f51cb9fa315f6a645859e00f6303c3
Thomas Gehrmann <s6thgehr@gmail.com>
2022-12-23 09:19:04 +0100

    Add OnlyLoggedInModal

--- pages/index.js
+++ pages/index.js
@@ -8,66 +8,88 @@ import { useState } from "react";
 import BikeType from "../utils/bike_type";
 import { useBikes } from "../hooks/useBikes";
 import { useBikeStore } from "../stores/useBikeStore";
+import OnlyLoggedInModal from "../components/OnlyLoggedInModal";
+import { useUserStore } from "../stores/useUserStore";
 
 export default function Home() {
     // const { bikes, error, isLoading } = useBikes();
     const bikes = useBikeStore((state) => state.bikes);
+    const [showModal, setShowModal] = useState(false);
+    const isLoggedIn = useUserStore((state) => state.isLoggedIn);
 
     const [filter, setFilter] = useState({});
 
     return (
-        <div className="bg-white">
-            <div className="mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
-                <h2 className="mb-4 text-2xl font-bold tracking-tight text-gray-900">
-                    Bikes for Sale
-                </h2>
-                <Filter filter={filter} setFilter={setFilter} />
+        <>
+            <div className="bg-white">
+                <div className="mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
+                    <h2 className="mb-4 text-2xl font-bold tracking-tight text-gray-900">
+                        Bikes for Sale
+                    </h2>
+                    <Filter filter={filter} setFilter={setFilter} />
 
-                <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:gap-x-8">
-                    {bikes
-                        ?.filter((bike) => bike.itemState === "published")
-                        .filter((bike) => {
-                            const selected = Object.keys(filter).filter(
-                                (key) => filter[key]
-                            );
-                            if (selected.length) {
-                                return selected.includes(bike.type);
-                            } else {
-                                return true;
-                            }
-                        })
-                        .map((bike) => (
-                            <div key={bike.id} className="group relative">
-                                <div className="min-h-80 aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-md bg-gray-200 group-hover:opacity-75 lg:aspect-none lg:h-80">
-                                    <img
-                                        src={bike.imageUrl}
-                                        alt="bike image"
-                                        className="h-full w-full object-cover object-center lg:h-full lg:w-full"
-                                    />
-                                </div>
-                                <div className="mt-4 flex justify-between">
-                                    <div>
-                                        <h3 className="text-sm text-gray-700">
-                                            <Link href={`/details/${bike.id}`}>
-                                                <span
-                                                    aria-hidden="true"
-                                                    className="absolute inset-0"
-                                                />
-                                                {bike.model}
-                                            </Link>
-                                        </h3>
-                                        <p className="mt-1 text-sm text-gray-500">
-                                            {bike.type}
+                    <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:gap-x-8">
+                        {bikes
+                            ?.filter((bike) => bike.itemState === "published")
+                            .filter((bike) => {
+                                const selected = Object.keys(filter).filter(
+                                    (key) => filter[key]
+                                );
+                                if (selected.length) {
+                                    return selected.includes(bike.type);
+                                } else {
+                                    return true;
+                                }
+                            })
+                            .map((bike) => (
+                                <div
+                                    key={bike.id}
+                                    className="group relative"
+                                    onClick={() => {
+                                        if (!isLoggedIn) {
+                                            setShowModal(true);
+                                        }
+                                    }}
+                                >
+                                    <div className="min-h-80 aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-md bg-gray-200 group-hover:opacity-75 lg:aspect-none lg:h-80">
+                                        <img
+                                            src={bike.imageUrl}
+                                            alt="bike image"
+                                            className="h-full w-full object-cover object-center lg:h-full lg:w-full"
+                                        />
+                                    </div>
+                                    <div className="mt-4 flex justify-between">
+                                        <div>
+                                            <h3 className="text-sm text-gray-700">
+                                                <Link
+                                                    href={
+                                                        isLoggedIn
+                                                            ? `/details/${bike.id}`
+                                                            : ""
+                                                    }
+                                                    scroll={isLoggedIn}
+                                                >
+                                                    <span
+                                                        aria-hidden="true"
+                                                        className="absolute inset-0"
+                                                    />
+                                                    {bike.model}
+                                                </Link>
+                                            </h3>
+                                            <p className="mt-1 text-sm text-gray-500">
+                                                {bike.type}
+                                            </p>
+                                        </div>
+                                        <p className="text-sm font-medium text-gray-900">
+                                            {bike.price} EUR
                                         </p>
                                     </div>
-                                    <p className="text-sm font-medium text-gray-900">
-                                        {bike.price} EUR
-                                    </p>
                                 </div>
-                            </div>
-                        ))}
+                            ))}
+                    </div>
                 </div>
             </div>
-        </div>
+            <OnlyLoggedInModal show={showModal} setShow={setShowModal} />
+        </>
     );
 }
